11df843cff74ce74b3b8e7af7acf03919fbe1edf
Enis Simsar <enis.simsar@monitise.com>
2017-07-07 16:27:32 +0300

    no message

--- S02.03-Exercise-Polish/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S02.03-Exercise-Polish/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -21,8 +21,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.android.sunshine.data.SunshinePreferences;
@@ -35,9 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherTextView;
 
-    TextView mErrorMessageTextView;
+    // TODO (6) Add a TextView variable for the error message display
 
-    ProgressBar mLoadingProgressBar;
+    // TODO (16) Add a ProgressBar variable to show and hide the progress bar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,9 +48,9 @@ public class MainActivity extends AppCompatActivity {
          */
         mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
 
-        mErrorMessageTextView = (TextView) findViewById(R.id.error_message);
+        // TODO (7) Find the TextView for the error message using findViewById
 
-        mLoadingProgressBar = (ProgressBar) findViewById(R.id.progress);
+        // TODO (17) Find the ProgressBar using findViewById
 
         /* Once all of our views are setup, we can load the weather data. */
         loadWeatherData();
@@ -63,31 +61,18 @@ public class MainActivity extends AppCompatActivity {
      * background method to get the weather data in the background.
      */
     private void loadWeatherData() {
-        showWeatherDataView();
-
+        // TODO (20) Call showWeatherDataView before executing the AsyncTask
         String location = SunshinePreferences.getPreferredWeatherLocation(this);
         new FetchWeatherTask().execute(location);
     }
 
-    private void showWeatherDataView(){
-        mErrorMessageTextView.setVisibility(View.INVISIBLE);
+    // TODO (8) Create a method called showWeatherDataView that will hide the error message and show the weather data
 
-        mWeatherTextView.setVisibility(View.VISIBLE);
-    }
-
-    private void showErrorMessage(){
-        mWeatherTextView.setVisibility(View.INVISIBLE);
-
-        mErrorMessageTextView.setVisibility(View.VISIBLE);
-    }
+    // TODO (9) Create a method called showErrorMessage that will hide the weather data and show the error message
 
     public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
 
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mLoadingProgressBar.setVisibility(View.VISIBLE);
-        }
+        // TODO (18) Within your AsyncTask, override the method onPreExecute and show the loading indicator
 
         @Override
         protected String[] doInBackground(String... params) {
@@ -117,11 +102,10 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String[] weatherData) {
-
-            mLoadingProgressBar.setVisibility(View.INVISIBLE);
+            // TODO (19) As soon as the data is finished loading, hide the loading indicator
 
             if (weatherData != null) {
-                showWeatherDataView();
+                // TODO (11) If the weather data was not null, make sure the data view is visible
                 /*
                  * Iterate through the array and append the Strings to the TextView. The reason why we add
                  * the "\n\n\n" after the String is to give visual separation between each String in the
@@ -130,9 +114,9 @@ public class MainActivity extends AppCompatActivity {
                 for (String weatherString : weatherData) {
                     mWeatherTextView.append((weatherString) + "\n\n\n");
                 }
-            } else {
-                showErrorMessage();
             }
+            // TODO (10) If the weather data was null, show the error message
+
         }
     }
 
